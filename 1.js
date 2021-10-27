2bdccc0832995e8587ae0cb0adb218d3475715b9
MMatasG <mmatasgaleote@gmail.com>
2021-10-27 12:07:56 +0200

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,16 @@
+alert("Desde el fichero");
+
+console.warn("Aviso uwu");
+
+
+var mivariable2 = 5;
+const mivariableconstante = "Variable constante";
+console.log(mivariable2, mivariableconstante);
+
+if (true) 
+{ 
+    var mivariable = 999;
+}
+
+console.log(mivariable);
+document.write("Variable:"+mivariable);
